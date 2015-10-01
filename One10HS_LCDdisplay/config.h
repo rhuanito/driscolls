d2a111a0c3efbc775d9dd45789570b3d448f0acc
Rhuanito Soranz Ferrarezi <rhuanito@terra.com.br>
2015-10-01 07:41:00 -0700

    Create config.h

---
+++ One10HS_LCDdisplay/config.h
@@ -0,0 +1,6 @@
+#define PROJECTID "000001"
+#define ARDUINOID "000003"
+
+#define INTERVAL_UNIT 1     //  SEC = 1, MIN = 2, HOUR = 3
+#define READING_INTERVAL 5 // Number of minutes or seconds between readings, must evenly divide 60 (1,2,3,4,5,6,10,12,15,20,30,60)
+                            // If INTERVAL_TYPE is Hours the value must evenly divide 24 (1,2,3,4,6,8,12,24)
